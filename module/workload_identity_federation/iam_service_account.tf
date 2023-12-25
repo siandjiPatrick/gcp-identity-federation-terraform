5ba6ea5f5bf0f4092234f003168157574e105b2d
patricksiandji <patrick.djumedio@pcg.io>
2023-12-25 06:38:36 +0100

    bindind a role to principalSet and updated git flow pipeline

--- module/workload_identity_federation/iam_service_account.tf
+++ module/workload_identity_federation/iam_service_account.tf
@@ -15,16 +15,8 @@ resource "google_project_iam_member" "github_srv_with_run_serviceAgent_role" {
 resource "google_service_account_iam_binding" "oidc_provider_role_for_srv_account" {
   service_account_id = google_service_account.github_service_account.name
   role               = "roles/iam.workloadIdentityUser"
-  members             = [
-   "principal://iam.googleapis.com/projects/142963639085/locations/global/workloadIdentityPools/github-pool/subject/assertion.sub",
-   #"principal://iam.googleapis.com/projects/142963639085/locations/global/workloadIdentityPools/github-pool/attribute.repository/siandjiPatrick/gcp-workload-identity-federation-with-terraform"
-#  "principal://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.identity_pool.name}/subject/assertion.sub",
-                     #   "principal://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.identity_pool.name}/attribute.repository/${var.github_username}/${var.github_repository}"
-                        ]
+  members = [
+    "principal://iam.googleapis.com/projects/142963639085/locations/global/workloadIdentityPools/github-pool/subject/assertion.sub",
+    "principalSet://iam.googleapis.com/projects/142963639085/locations/global/workloadIdentityPools/github-pool/attribute.repository/siandjiPatrick/gcp-workload-identity-federation-with-terraform"
+  ]
 }
-
-# resource "google_service_account_iam_member" "oidc_provider_repo_role_for_srv_account" {
-#   service_account_id = google_service_account.github_service_account.name
-#   role               = "roles/iam.workloadIdentityUser"
-#   member             = "principal://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.identity_pool.name}/attribute.repository/${var.github_username}/${var.github_repository}"
-# }
